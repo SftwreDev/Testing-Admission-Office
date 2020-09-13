8a080c5eaa0b346838b81bd5b55ccae4c30cab07
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2020-09-13 10:05:32 +0800

    Update settings.py

--- taomanagementsystem/settings.py
+++ taomanagementsystem/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'sq76tb)nwkm+ptfn$ch)m1u@o0(s$ykkw$a+_=!10i!fi!-#+&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['testing-admission-office.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testing-admission-office.herokuapp.com']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
